02d8b08f3d95c4d3fd4e056c2f58e04061737669
cgcaruzo <cgcaruzo@gmail.com>
2019-03-19 18:56:01 -0300

    ejercicio 2 resuelto

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -2,4 +2,18 @@
 
 
 def mayor(a, b, c):
-    pass
+    if a > b :
+        if a > c :
+            return a
+        else :
+            return c
+
+    else :
+        if b > c:
+            return b
+        else :
+            return c
+
+assert(mayor(3,2,1) == 3)
+assert(mayor(1,3,2) == 3)
+assert(mayor(1,2,3) == 3)
