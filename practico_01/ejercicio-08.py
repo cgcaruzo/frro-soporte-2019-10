e621070a4cff2b3fc923483da2fc0a9d85f5f168
Claudio Caruzo <ccaruzo@grupotyt.com.ar>
2019-03-27 16:04:23 -0300

    ejercicio 8

--- practico_01/ejercicio-08.py
+++ practico_01/ejercicio-08.py
@@ -1,8 +1,13 @@
-# Implementar la función mitad(), que devuelve la mitad de palabra.
-# Si la longitud es impar, redondear hacia arriba.
+# Definir una función superposicion() que tome dos listas y devuelva True si tienen al
+# menos 1 miembro en común o devuelva False de lo contrario. Escribir la función
+# usando el bucle for anidado.
 
+def superposicion(lista1, lista2):
+    for x in lista1:
+    	for y in lista2:
+    		if x == y:
+    			return True
+    return False
 
-# hola -> ho
-# verde -> ver
-def mitad(palabra):
-    pass
+assert(superposicion([1,2,3,4,5], [9,6,7,8]) == False)
+assert(superposicion([1,2,3,4,5], [4,6,7,8]) == True)
