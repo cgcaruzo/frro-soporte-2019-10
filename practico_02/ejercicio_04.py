cca378a0faf8e0643c314165b4bb73761e9dab00
Claudio Caruzo <ccaruzo@grupotyt.com.ar>
2019-04-03 15:44:18 -0300

    ejercicio 04

--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -7,16 +7,24 @@
 # Métodos:
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
+from datetime import datetime
+from ejercicio_03 import Persona
 
-
-class Estudiante:
+class Estudiante(Persona):
 
     def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
-        pass
+        self.carrera = carrera
+        self.anio = anio
+        self.cantidad_materias = cantidad_materias
+        self.cantidad_aprobadas = cantidad_aprobadas
 
     def avance(self):
-        pass
+        print(self.cantidad_aprobadas *100 / self.cantidad_materias)
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
+        print(self.edad - (datetime.now().year - self.anio))
         pass
+
+e = Estudiante('ISI', 2008, 35, 25)
+e.edad = 25
