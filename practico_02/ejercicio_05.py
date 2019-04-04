5863ab0dff3536c714f6610aaf6cc14f10cf2c58
cgcaruzo <cgcaruzo@gmail.com>
2019-04-04 18:11:10 -0300

    Update ejercicio_05.py

--- practico_02/ejercicio_05.py
+++ practico_02/ejercicio_05.py
@@ -5,4 +5,28 @@ from practico_02.ejercicio_04 import Estudiante
 
 
 def organizar_estudiantes(estudiantes):
-    pass
+    diccionario = {}
+    for estudiante in estudiantes:
+        if estudiante.carrera in diccionario:
+            diccionario[estudiante.carrera] = diccionario[estudiante.carrera] + 1
+        else:
+            diccionario[estudiante.carrera] = 1
+    return diccionario
+
+a = Estudiante('ISI', 2008, 35, 25)
+b = Estudiante('ISI', 2008, 35, 25)
+c = Estudiante('ISI', 2008, 35, 25)
+d = Estudiante('ISI', 2008, 35, 25)
+e = Estudiante('QUIMICA', 2008, 35, 25)
+f = Estudiante('QUIMICA', 2008, 35, 25)
+g = Estudiante('CIVIL', 2008, 35, 25)
+h = Estudiante('CIVIL', 2008, 35, 25)
+i = Estudiante('CIVIL', 2008, 35, 25)
+j = Estudiante('MECANICA', 2008, 35, 25)
+k = Estudiante('ELECTRICA', 2008, 35, 25)
+l = Estudiante('ELECTRICA', 2008, 35, 25)
+
+lista = [a,b,c,d,e,f,g,h,i,j,k,l]
+
+
+assert(organizar_estudiantes(lista) == {'ISI': 4, 'QUIMICA': 2, 'CIVIL': 3, 'MECANICA': 1, 'ELECTRICA': 2})
