1fba47c8a79d18e8db60a0f8ea75f04dd1575423
cgcaruzo <cgcaruzo@gmail.com>
2019-05-30 17:47:58 -0300

    ejercicio 3

--- practico_03a/ejercicio_03.py
+++ practico_03a/ejercicio_03.py
@@ -2,18 +2,36 @@
 # Devuelve un booleano en base a si encontro el registro y lo borro o no.
 
 import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from practico_03a.ejercicio_01 import Persona
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
+from practico_03a.ejercicio_01 import reset_tabla
+from practico_03a.ejercicio_02 import agregar_persona
 
+
+Base = declarative_base() # Metadatos
+engine = create_engine('sqlite:///mibase.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
 
 def borrar_persona(id_persona):
-    return False
-
+    persona = session.query(Persona).filter_by(IdPersona=id_persona).first()
+    if persona:
+        session.delete(persona)
+        session.commit()
+        return True
+    else:
+        return False
 
 @reset_tabla
 def pruebas():
-    assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
+    idpersona=agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    assert borrar_persona(idpersona)
     assert borrar_persona(12345) is False
 
 if __name__ == '__main__':
